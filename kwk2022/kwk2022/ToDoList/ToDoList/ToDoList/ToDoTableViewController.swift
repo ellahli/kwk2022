7fc1b42bdc24128fd211cf52e8409247d159154c
Ella Li <62983668+ellahli@users.noreply.github.com>
2022-08-16 11:00:51 -0400

    Add files via upload

---
+++ kwk2022/kwk2022/ToDoList/ToDoList/ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by scholar on 8/12/22.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var listOfToDo : [ToDoCD] = []
+    
+    func getToDos() {
+        if let accessToCoreData = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            if let dataFromCoreData = try?
+                accessToCoreData.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                listOfToDo = dataFromCoreData
+                tableView.reloadData()
+                
+            }
+        }
+    }
+    
+    
+   /* func createToDo() -> [ToDoClass] {
+        
+        let swiftToDo = ToDoClass()
+        swiftToDo.description = "Learn Swift"
+        swiftToDo.important = true
+        
+        let dogToDo = ToDoClass()
+        dogToDo.description = "Walk the dog"
+        
+        return [swiftToDo, dogToDo]
+        
+    } // end of createToDo function*/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       // listOfToDo = createToDo()
+        
+        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return listOfToDo.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        let eachToDo = listOfToDo[indexPath.row]
+        
+            if let thereIsDescription = eachToDo.descriptionInCD {
+                if eachToDo.importantInCD {
+                    cell.textLabel?.text = "🍄" + thereIsDescription}
+                else {
+                    cell.textLabel?.text = eachToDo.descriptionInCD
+                }
+            }
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated : Bool) {
+        getToDos()
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let nextAddToDoVC = segue.destination as? AddToDoViewController {
+            nextAddToDoVC.previousToDoTVC = self
+        }
+        
+        if let nextCompletedToDoVC = segue.destination as? CompletedToDoViewController {
+            if let choosenToDo = sender as? ToDoCD {
+                nextCompletedToDoVC.selectedToDo = choosenToDo
+                nextCompletedToDoVC.previousToDoTVC = self
+            }
+        }
+            
+    }
+    override func tableView(_ tableView: UITableView,
+    didSelectRowAt indexPath: IndexPath) {
+        let eachToDo = listOfToDo[indexPath.row]
+        
+        performSegue(withIdentifier: "moveToCompletedToDoVC",
+                     sender: eachToDo)
+    }
+    
+
+}
